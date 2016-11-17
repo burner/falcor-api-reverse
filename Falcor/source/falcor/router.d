7dd32301a8d3441162ce0572166cf868f1d72c9a
Robert burner Schadek <rburners@gmail.com>
2016-11-17 17:27:42 +0100

    some insert

--- Falcor/source/falcor/router.d
+++ Falcor/source/falcor/router.d
@@ -1,7 +1,16 @@
 module falcor.router;
 
+import falcor.hashtable;
+import falcor.types;
+
 class Router {
+	HashTable ht;
 	this() {
 
+	}
+
+	void insert(string path, Callback cb) {
+		this.ht.insert(StrDel(path, cb));
+		this.ht.rebuildHashes();
 	}
 }
