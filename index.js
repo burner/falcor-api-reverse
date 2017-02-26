d2731700e893cf9a9baefb627a204f9f88f8932e
Robert burner Schadek <rburners@gmail.com>
2017-02-27 00:24:48 +0100

    more tickering

--- index.js
+++ index.js
@@ -46,7 +46,28 @@ function example(){
 app.use('/model.json', falcorExpress.dataSourceRoute(function (req, res) {
   // create a Virtual JSON resource with single key ("greeting")
 	console.log(req.query);
-    return new Router( example() );
+    return new Router([
+		{
+			route: "greetings[{integers:idx}]",
+			get: function(pathSet) {
+				if(pathSet.idx == 0) {
+					return { path : ["greetings", pathSet.idx],
+                 	  value : {
+                     	name: "Hello World",
+					 	value: "Foobar"
+                 	  }
+					}
+				} else if(pathSet.idx == 1) {
+					return { path : ["greetings", pathSet.idx],
+                 	  value : {
+                     	name: "Hello Vibe",
+					 	value: "args"
+                 	  }
+					}
+				}
+			}
+		}
+	]);
 }));
 
 // serve static files from current directory
